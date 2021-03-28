29246bd299a379a3cfedb78cf63b7a2d119a5ba1
yizec2 <yizec2@illinois.edu>
2021-03-28 15:31:27 +0800

    rtc done 3.2

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -5,23 +5,15 @@
     Outputs: none 
     func: init the rtc 
 */
-volatile uint32_t rtc_counter;
+//volatile uint32_t rtc_counter;
+static volatile uint32_t rtc_counter;
 
-void RTC_INIT(void)
+int32_t rtc_open()
 {
     uint8_t prev;
     cli();
-    /* frequency define */
-    /* select the Register A and disable NMI*/
-    outb(REGA_OFF, PORT_70);
-    outb(0x20,PORT_CMOS); //???????????????????
-    /* read the current value in Reg_A */
-    outb(REGA_OFF, PORT_70);
-    prev = inb(PORT_CMOS);
-    /* since inb would move RegD so agagin oub */
-    outb(REGA_OFF, PORT_70);
-    outb(((prev & 0xF0) | INIT_FREQ), PORT_CMOS);
 
+    rtc_write(RTC_INIT_F);
     /* select the Register B and disable NMI*/
     outb((REGB_OFF | RTC_NMI), PORT_70);
 
@@ -34,7 +26,7 @@ void RTC_INIT(void)
 
     sti();
     enable_irq(IRQ_RTC_NUM);
-    
+    return 0;
 }
 
 
@@ -50,9 +42,54 @@ void RTC_handler(void)
     outb(REGC_OFF,PORT_70);
     /* just read the data */
     inb(PORT_CMOS);
-    //rtc_counter++;
+    // counter counts up
+    rtc_counter++;
+    rtc_counter %= CNT_MAX;
     //test_interrupts();
     send_eoi(IRQ_RTC_NUM);
 
 }
 
+/* int32_t rtc_write()
+ * Inputs: lg_freq -- the lg(frequency)
+ * Outputs: none
+ * We need to write the frequency to the RTC, and return 0/-1 to see if okay
+ * the frequency set
+ */
+int32_t rtc_write(int32_t lg_freq)
+{
+    uint32_t prev;
+    // don't accept frequecy larger than 1024Hz
+    if (lg_freq > 10){
+        return -1;
+    }
+    // argument to rtc chip
+    uint8_t rate = FREQ2RATE(lg_freq);
+    /* read the current value in Reg_A */
+    outb(REGA_OFF, PORT_70);
+    prev = inb(PORT_CMOS);
+    /* since inb would move RegD so agagin oub */
+    outb(REGA_OFF, PORT_70);
+    outb(((prev & 0xF0) | rate), PORT_CMOS);
+    return 0;
+}
+
+/* int32_t rtc_read() : return until next rtc interrupt
+ * Inputs: none
+ * Outputs: none
+ * Side effect: nothing
+ */
+int32_t rtc_read(){
+    int32_t old_val = rtc_counter;
+    while (old_val == rtc_counter);
+    return 0;
+}
+
+/* int32_t rtc_close()
+ * Inputs: none
+ * Outputs: none
+ * Side effect: nothing
+ */
+int32_t rtc_close(){
+    return 0;
+}
--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -11,28 +11,34 @@
 #define REGB_OFF    0xB
 #define REGC_OFF    0xC
 
-#define INIT_FREQ   0xF
+//#define INIT_FREQ   0xF
 #define PORT_70     0x70
 #define PORT_CMOS   0x71
 
 #define RTC_NMI     0x80
 #define RTC_PIE     0x40
-
+#define CNT_MAX     0x80000000
 #define IRQ_RTC_NUM 8
 
-#define RTC_INIT_F  2
-
-
-
+#define RTC_INIT_F  1       // initial frequency, 2^1
+#define FREQ2RATE(freq) 16 - freq
 
 #ifndef ASM
 
 #include "lib.h"
-/* init the rtc */
-extern void RTC_INIT(void);
-extern volatile uint32_t rtc_counter;
+//extern volatile uint32_t rtc_counter;
+
 /* RTC handler */
 extern void RTC_handler(void);
+/* void RTC_frequency_set */
+extern int32_t rtc_write(int32_t lg_freq);
+/* void RTC open */
+extern int32_t rtc_open();
+/* void RTC_close */
+extern int32_t rtc_close();
+/* void RTC_frequency_set */
+extern int32_t rtc_read();
+
 #endif
 
 #endif 
